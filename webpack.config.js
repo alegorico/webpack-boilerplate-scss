cc71b458fcb67abc5b279096c9f361e4fdecdf47
Alejandro G <alegorico@icloud.com>
2018-03-05 01:43:38 +0100

    cambio de directorio de estaticos html

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: "./assets/index.html",
       filename: "./index.html"
     })
   ]
