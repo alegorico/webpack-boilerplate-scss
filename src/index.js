35bed81ad2441a101bcc955e5309b8e7efd26768
Alejandro G <mierdeneitor@gmail.com>
2018-03-11 20:28:26 +0100

    actualizacion de punto de acceso a la app

--- src/index.js
+++
@@ -1,6 +0,0 @@
-var app = document.getElementById('app');
-import mensajes from "./components/mensajes/mensajes";
-import css from "./stylesheets/main.scss";
-
-
-app.innerHTML = "<p>" + mensajes.saludo + " " + mensajes.lugar + "</p>";
