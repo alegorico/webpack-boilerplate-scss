b084663a57560af2c8d6f99b1861b151ada55df4
Alejandro G <mierdeneitor@gmail.com>
2018-03-12 19:07:25 +0100

    codigo duplicado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 var app = document.getElementById('app');
 import mensajes from "./components/mensajes/mensajes";
-import css from "./stylesheets/main.scss";
 import txt from "./contents/menssage.pug";
 
 
